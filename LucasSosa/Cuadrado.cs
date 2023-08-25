5168eb1401da3c876a2e103701ad7e8f28bb2942
LucasSosa20 <143140422+LucasSosa20@users.noreply.github.com>
2023-08-24 21:26:15 -0300

    Add files via upload

---
+++ LucasSosa/Cuadrado.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+
+namespace LucasSosa
+{
+    
+    
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Console.Write("Ingrese el tamaño del lado del cuadrado: ");
+                int lado = int.Parse(Console.ReadLine());
+
+                if (lado > 0)
+                {
+                    for (int i = 1; i <= lado; i++)
+                    {
+                        for (int j = 1; j <= lado; j++)
+                        {
+                            if (i == 1 || i == lado || j == 1 || j == lado)
+                            {
+                                Console.Write("*");
+                            }
+                            else
+                            {
+                                Console.Write(" ");
+                            }
+                        }
+                        Console.WriteLine();
+                    }
+
+                Console.WriteLine();
+            }
+                else
+                {
+                    Console.WriteLine("El tamaño del lado debe ser mayor que 0.");
+                }
+            }
+        }
+    
+
+}
+
---
+++ LucasSosa/RelojHorarioActual.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+
+namespace DigitalClock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                DateTime hora = DateTime.Now;
+                Console.Clear();  // Limpiar la consola para mostrar la hora actualizada
+
+                Console.WriteLine("Reloj Digital");
+                Console.WriteLine("---------------------");
+                Console.WriteLine($"{hora:HH:mm:ss}");
+                Console.WriteLine("---------------------");
+
+                Thread.Sleep(1000);  // Espera 1 segundo
+            }
+        }
+    }
+}
+
+
